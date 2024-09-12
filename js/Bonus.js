1b87a6148e381b335ae543da434f2bb51b3247b6
Yuri Pourre <yuripourre@users.noreply.github.com>
2024-09-11 17:08:56 -0700

    Remove magic numbers

--- js/Bonus.js
+++ js/Bonus.js
@@ -7,14 +7,16 @@ Bonus = Entity.extend({
 
     init: function(position, typePosition) {
         this.type = this.types[typePosition];
-
+        
         this.position = position;
 
+        var tileSize = gGameEngine.tileSize;
+        
         this.bmp = new createjs.Bitmap(gGameEngine.bonusesImg);
         var pixels = Utils.convertToBitmapPosition(position);
         this.bmp.x = pixels.x;
         this.bmp.y = pixels.y;
-        this.bmp.sourceRect = new createjs.Rectangle(typePosition * 32, 0, 32, 32);
+        this.bmp.sourceRect = new createjs.Rectangle(typePosition * gGameEngine.tileSize, 0, tileSize, tileSize);
         gGameEngine.stage.addChild(this.bmp);
     },
 
